4f09202027bac2d249e777790ef042718bd05dbc
kducasse <karaducasse@gmail.com>
2020-10-09 13:23:27 -0400

    Final Backend Code

--- src/main/java/com/launchacademy/seeders/Seeder.java
+++ src/main/java/com/launchacademy/seeders/Seeder.java
@@ -1,7 +1,12 @@
 package com.launchacademy.seeders;
 
 import com.google.common.collect.Lists;
+import com.launchacademy.models.AdoptablePet;
+import com.launchacademy.models.PetType;
+import com.launchacademy.repositories.AdoptablePetRepository;
+import com.launchacademy.repositories.PetTypeRepository;
 import java.util.Collection;
+import java.util.Map;
 import org.springframework.data.repository.CrudRepository;
 
 public class Seeder {
@@ -12,5 +17,22 @@ public class Seeder {
         repository.save(data);
       }
     }
+  }
+
+  static public void seedPets(PetTypeRepository petTypeRepo, AdoptablePetRepository adoptablePetRepo,
+      Map<AdoptablePet, String> adoptablePets) {
+    if(Lists.newArrayList(adoptablePetRepo.findAll()).isEmpty()) {
+      PetType twoLegged = petTypeRepo.findByTypeIgnoreCase("Two-legged");
+      PetType fourLegged = petTypeRepo.findByTypeIgnoreCase("Four-legged");
+
+      for (AdoptablePet pet : adoptablePets.keySet()) {
+        if (adoptablePets.get(pet).equals("two legged")) {
+          pet.setPetType(twoLegged);
+        } else {
+          pet.setPetType(fourLegged);
+        }
+      }
+    }
+    seed(adoptablePetRepo, adoptablePets.keySet());
   }
 }
